eb14e1b8a32e318b95af951cad9ffd48d6f506ae
harry <zh_harry@163.com>
2018-02-07 19:07:53 +0800

    feat:add sparrow markdown module

--- src/main/java/com/sparrow/markdown/parser/impl/MarkdownParserComposite.java
+++ src/main/java/com/sparrow/markdown/parser/impl/MarkdownParserComposite.java
@@ -3,15 +3,17 @@ package com.sparrow.markdown.parser.impl;
 import com.sparrow.markdown.mark.MARK;
 import com.sparrow.markdown.mark.MARK_STATUS;
 import com.sparrow.markdown.parser.MarkParser;
-import com.sparrow.support.protocol.pager.PagerResult;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by harry on 2018/2/6.
  */
 public class MarkdownParserComposite implements MarkParser {
+
+    private static final int MAX_MARK_LENGTH=7;
 
     private String content;
 
@@ -40,12 +42,27 @@ public class MarkdownParserComposite implements MarkParser {
 
     private List<MarkParser> markParserList = new ArrayList<MarkParser>();
 
+    private String readMaxMark(){
+          return new String(Arrays.copyOfRange(this.contentCharArray,this.currentPointer,this.currentPointer+MAX_MARK_LENGTH));
+    }
+
+    private String readLine(){
+
+    }
     public MarkdownParserComposite(String content) {
         this.content = content;
     }
 
     private MARK detectStartMark() {
-        char current=this.contentCharArray[this.currentPointer];
+        if(this.currentDetectingMark.length()==0){
+            if(this.currentPointer!=0||(this.currentPointer>2)){
+                if(this.currentPointer)
+            }
+
+            ||(this.currentPointer>2&&this.contentCharArray[])
+        }
+        if(this.currentPointer==0||)
+        currentDetectingMark.append(this.contentCharArray[this.currentPointer]);
         for (MARK mark : MARK.CONTAINER) {
             if (mark.getStart().startsWith(current))) {
                 currentDetectingMark.delete()
