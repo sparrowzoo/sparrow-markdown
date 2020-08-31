2e186530a42ff6ccfcc57ccc468be9ae4b6a052d
zhanglizhi01 <zhanglizhi01@meicai.cn>
2020-08-31 10:16:39 +0800

    modify sparrow orm

---
+++ src/main/java/com/sparrow/markdown/controller/MarkdownPreviewController.java
@@ -0,0 +1,19 @@
+package com.sparrow.markdown.controller;
+
+import com.sparrow.markdown.mark.MarkContext;
+import com.sparrow.markdown.parser.MarkParser;
+import com.sparrow.markdown.parser.impl.MarkdownParserComposite;
+import com.sparrow.markdown.vo.MarkdownVO;
+import com.sparrow.utility.StringUtility;
+
+public class MarkdownPreviewController {
+    public MarkdownVO preview(String markdown) {
+        if (StringUtility.isNullOrEmpty(markdown)) {
+            return new MarkdownVO("");
+        }
+        MarkContext markContext = new MarkContext(markdown);
+        MarkParser markParser = MarkdownParserComposite.getInstance();
+        markParser.parse(markContext);
+        return new MarkdownVO(markContext.getHtml());
+    }
+}
